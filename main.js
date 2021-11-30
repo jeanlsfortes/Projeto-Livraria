d2f3a133affad3b5b668deadca71864ee4a0617b
jeanls-solano <67402140+jeanls-solano@users.noreply.github.com>
2021-11-30 11:19:03 -0400

    up (cadastro e login)
    efetuei a criação das telas de cadastro e login, só a parte de html e css.

---
+++ main.js
@@ -0,0 +1,19 @@
+function validar(){
+    alert("naej");
+    return false;
+    /*erro na funcão
+    var senha = CadastroNovoCliente.senha.Value;
+    var confirmarSenha = CadastroNovoCliente.confirmarSenha.Value;
+
+    if(senha == "" || senha.length <= 5){
+        alert("Preencha o campo com no mínimo 6 caracteres ")
+        CadastroNovoCliente.senha.focus();
+        return false;
+    }
+    if(senha != confirmarSenha){
+        alert("As senhas difitadas são diferentes")
+    }
+    */
+
+}
+
